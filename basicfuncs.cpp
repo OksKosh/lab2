ddc63bc6ba36b4d5a0c4e347f785263f8da4df57
Oks Kosh <oksana.pavletsova.00@bk.ru>
2020-10-26 15:59:17 +0300

    feat: get_system_info()

--- basicfuncs.cpp
+++ basicfuncs.cpp
@@ -1,7 +1,29 @@
 #include "basicfuncs.h"
 
+#include <iostream>
+#include <bitset>
+
 void get_system_info() {
+	SYSTEM_INFO info;
+	GetSystemInfo(&info);
 	
+	for (auto el: PROCESSOR_ARCHITECTURE) {
+	    if (el.first == info.wProcessorArchitecture) {
+	        std::cout << "The processor architecture is " << el.second << "\n";
+	    }
+	}
+	
+	std::cout << "Page size: " << info.dwPageSize << "\n";
+	std::cout << "Pointer to the lowest adress accesible: ";
+	std::cout << info.lpMinimumApplicationAddress << "\n";
+	std::cout << "Pointer to the highest adress accesible: ";
+	std::cout << info.lpMaximumApplicationAddress << "\n";
+	std::cout << "Bitmask of set of processors: ";
+	std::cout << std::bitset<8 * sizeof(DWORD)>(info.dwActiveProcessorMask) << "\n";
+	std::cout << "Number of processors: " << info.dwNumberOfProcessors << "\n";
+	std::cout << "Allocation granularity: " << info.dwAllocationGranularity << "\n";
+	std::cout << "Processor level: " << info.wProcessorLevel << "\n";
+	std::cout << "Processor revision: " << info.wProcessorRevision << "\n";
 }
 
 void get_memory_status() {
--- basicfuncs.h
+++ basicfuncs.h
@@ -1,6 +1,9 @@
 #ifndef BASICFUNCS_H
 #define BASICFUNCS_H
 
+#include <map>
+#include <windows.h>
+
 void get_system_info();
 void get_memory_status();
 void get_space_status();
@@ -9,5 +12,13 @@ void alocate_region();
 void write_to_space();
 void set_protect();
 void free_region();
-	
+
+std::map<WORD, std::string> const PROCESSOR_ARCHITECTURE = {
+	{9, "x64 (AMD or Intel)."},
+	{5, "ARM."},
+	{12, "ARM64."},
+	{6, "Intel Itanium-based."},
+	{0, "x86."},
+	{0xffff, "unknown."},
+};
 #endif //BASICFUNCS_H
